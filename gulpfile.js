dbcad11977427368fdd15427afa055d35090ef64
Thomas Leu <tomleu@gmail.com>
2016-04-19 09:24:03 -0500

    Add gulp and tasks to project

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
